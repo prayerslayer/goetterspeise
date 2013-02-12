f01a25991b3adde20fb0674f0dcfc87a8b709b58
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-02-12 11:15:51 +0100

    dynamic port

--- server/app.js
+++ server/app.js
@@ -11,4 +11,4 @@ var app = bogart.app();
 app.use(bogart.batteries); // A batteries included JSGI stack including streaming request body parsing, session, flash, and much more.
 app.use(router); // Our router
 
-app.start();
+app.start( process.env.PORT);
