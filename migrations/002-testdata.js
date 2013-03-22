f8ad7557a9ba9700163301944a6574257ac1252b
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-03-22 14:33:52 +0100

    more testdata

--- migrations/002-testdata.js
+++ migrations/002-testdata.js
@@ -3,7 +3,24 @@ var client = new db.Client( process.env.DB_URL );
 
 exports.up = function(next){
 	client.connect( function( error ) {
+
+		client.query( "INSERT INTO users ( email ) VALUES ( 'prayerslayer@gmail.com' ) ", function( err, result ) {
+			console.log( result );
+		});
+
+		client.query( "INSERT INTO tags ( name ) VALUES ( 'cool people' )", function( err, result ) {
+			console.log( result );
+		});
+
 		client.query( "INSERT INTO subscriptions ( name, url, feed_path ) VALUES ('npiccolotto', 'http://npiccolotto.com/', 'feed.xml')", function( err, result ) {
+			console.log( result );
+		});
+
+		client.query( "INSERT INTO user_tags ( user_id, tag_id ) VALUES ( 1, 1 )", function( err, result ) {
+			console.log( result );
+		});
+
+		client.query( "INSERT INTO tagged_subscriptions ( ut_id, subscription_id ) VALUES ( 1, 1 )", function( err, result ) {
 			console.log( result );
 			client.end();
 			next();
@@ -13,11 +30,22 @@ exports.up = function(next){
 
 exports.down = function(next){
 	client.connect( function( error ) {
-		client.query( "DELETE * FROM items", function( err, result ) {
+
+		client.query( "DELETE FROM user_items");
+
+		client.query( "DELETE FROM items", function( err, result ) {
 			console.log( result );
 		});
 
-		client.query( "DELETE FROM subscriptions WHERE name='npiccolotto'", function( erro, result ) {
+		client.query( "DELETE FROM tagged_subscriptions ");
+
+		client.query( "DELETE FROM user_tags" );
+
+		client.query( "DELETE FROM subscriptions ");
+
+		client.query( "DELETE FROM tags ");
+
+		client.query( "DELETE FROM users", function( erro, result ) {
 			console.log( result );
 			client.end();
 			next();
